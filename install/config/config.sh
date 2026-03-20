c4133de1cf48cf6f408962fc9516e55d2150a65a
unknown <142383240+anbe-on@users.noreply.github.com>
2026-03-20 17:22:15 +0700

    Initial commit

--- install/config/config.sh
+++ install/config/config.sh
@@ -4,3 +4,6 @@ cp -R ~/.local/share/omarchy/config/* ~/.config/
 
 # Use default bashrc from Omarchy
 cp ~/.local/share/omarchy/default/bashrc ~/.bashrc
+
+# Custom fff terminal file manager
+sudo curl -fsSL https://raw.githubusercontent.com/anbe-on/ffff/master/install.sh | bash
