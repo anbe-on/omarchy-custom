a7044b6ef31aeff6639e213be6551e8c26cb9010
unknown <142383240+anbe-on@users.noreply.github.com>
2026-03-28 16:00:59 +0700

    <fix> installation error finished.sh, <change> simplify plymouth/omarchy.script

--- install/post-install/finished.sh
+++ install/post-install/finished.sh
@@ -1,14 +1,14 @@
 stop_install_log
 
+echo_in_style() {
+  echo "$1" | tte --canvas-width 0 --anchor-text c --frame-rate 640 print
+}
+
 # Custom fff terminal file manager
 sudo curl -fsSL https://raw.githubusercontent.com/anbe-on/ffff/master/install.sh | bash
 
 # Install custom AUR packages without user confirmation
-yay -S --noconfirm cloudflare-warp-bin zen-browser-bin
-
-echo_in_style() {
-  echo "$1" | tte --canvas-width 0 --anchor-text c --frame-rate 640 print
-}
+yay -S --noconfirm ani-cli cloudflare-warp-bin zen-browser-bin
 
 clear
 echo
